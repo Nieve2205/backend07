ed44112ecf76bddfa69ea0e9fb542ef2b0b5d5e2
Nieve2205 <166433352+Nieve2205@users.noreply.github.com>
2025-05-05 10:04:54 -0500

    Segundo commit

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,15 +1,14 @@
-module.exports = {
-    HOST: process.env.DB_HOST,
-    USER: process.env.DB_USER,
-    PASSWORD: process.env.DB_PASSWORD,
-    DB: process.env.DB_NAME,
-    PORT: process.env.DB_PORT,
-    dialect: "postgres", // Cambiado a 'postgres' en lugar de 'mysql'
+export default {
+    HOST: "localhost",
+    USER: "root",
+    PASSWORD: "",
+    DB: "db",
+    PORT: 3306,
+    dialect: "mysql",
     pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
-    }
-  };
-  
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+};
